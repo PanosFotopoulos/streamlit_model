ab701f0d421163622ecd19ddbae2a2eb9b70ec10
PanosData <138224634+PanosFotopoulos@users.noreply.github.com>
2025-09-09 19:50:42 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -18,17 +18,18 @@ def preprocess_image(img):
     Accepts either:
       - a NumPy BGR image (as loaded by cv2.imread), or
       - a string path to an image file
-    Returns: array of shape (1, 60, 80, 1) float32 in [0,1]
+    Returns: array of shape (1, 100, 100, 1) float32 in [0,1]
     """
     if isinstance(img, str):
         img = cv2.imread(img)  # BGR
     if img is None:
         raise ValueError("preprocess_image: invalid image input (None)")
 
-    img = cv2.resize(img, (80, 60))
+    img = cv2.resize(img, (100, 100))
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = img.astype(np.float32) / 255.0
-    img = img[np.newaxis, :, :, np.newaxis]  # (1, 60, 80, 1)
+    img = img[np.newaxis, :, :, np.newaxis]  # (1, 100, 100, 1)
     return img
 
 __all__ = ["get_image_files", "preprocess_image"]
+
